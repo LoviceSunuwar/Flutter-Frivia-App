190cebf96ab89278601b0e73d3176281f0997d7a
LoviceSunuwar <lovicesunuwar@gmail.com>
2022-09-21 01:14:02 -0400

    fixed asset issue, created text widget for the gameUI

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,14 +1,40 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  double? _deviceHeight, _deviceWidth;
 
   @override
   Widget build(BuildContext context) {
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
     return _buildUI();
   }
 
   Widget _buildUI() {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+          child: Container(
+        padding: EdgeInsets.symmetric(
+          horizontal: _deviceHeight! * 0.15,
+        ),
+        child: _gameUI(),
+      )),
+    );
+  }
+
+  Widget _gameUI() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      mainAxisSize: MainAxisSize.max,
+      children: [_questionText()],
+    );
+  }
+
+  Widget _questionText() {
+    return const Text(
+      "Nothing intresting",
+      style: TextStyle(
+          color: Colors.black, fontSize: 25, fontWeight: FontWeight.w500),
+    );
   }
 }
